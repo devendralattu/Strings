753d1b91317cbd611f0419a9947a873f8a1c38e5
Devendra Lattu <dev12@umbc.edu>
2016-09-08 09:43:35 -0400

    Update ReverseWordsInString.java

--- ReverseWords/ReverseWordsInString.java
+++ ReverseWords/ReverseWordsInString.java
@@ -18,50 +18,45 @@ public class ReverseWordsInString {
 	}
 
 	public String reverseAllWords(char cStr[]) {
-		int i = 0, pointer = 0;
+		int i = 0;
 		int len = cStr.length;
-		int spaceArr[] = new int[len];
-		
-		//reverse the entire string: "eulb si yks ehT"
+
+		// reverse the entire string: "eulb si yks ehT"
 		cStr = reverseString(cStr, 0, len - 1);
 		System.out.println("initial reversed string = " + new String(cStr));
-		
-		//get location of spaces in string 
+
+		// get location of spaces in string
+		int start = 0, end = 0;
 		for (i = 0; i < len; i++) {
-			if(cStr[i] == ' '){
-				spaceArr[pointer++] = i;
+			if (cStr[i] == ' ') {
+				end = i;
+				cStr = reverseString(cStr, start, end - 1);
+				start = end + 1;
 			}
 		}
-		//pointer for the last word
-		spaceArr[pointer++] = i;
-		
-		//Perform reverse operations
-		int start = 0, end = 0;
-		for(i = 0; i < pointer; i++){
-			end = spaceArr[i];
-			cStr = reverseString(cStr, start, end - 1);
-			start = end + 1;
-		}
-		
+
+		// Perform reverse operations for last word
+		cStr = reverseString(cStr, start, len - 1);
+
 		return new String(cStr);
 	}
 
 	public static void main(String[] args) {
-		//The sky is blue
+		// The sky is blue
 		ReverseWordsInString r = new ReverseWordsInString();
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a string = ");
 		String str = in.nextLine();
 		str = str.trim();
 		char cStr[] = str.toCharArray();
-		
-		if(cStr.length == 0){
+
+		if (cStr.length == 0) {
 			System.out.println("Invalid String entered");
 			return;
 		}
 		String output = r.reverseAllWords(cStr);
 		System.out.println("Reversed words in string = " + output);
-		//blue is sky The
+		// blue is sky The
 	}
 
 }
